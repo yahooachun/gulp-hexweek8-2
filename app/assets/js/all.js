1286f5329f14261624954ab59c824cdb77a4f129
yahooachun <yahooachun@gmail.com>
2021-01-02 16:10:21 +0800

    first commit

---
+++ app/assets/js/all.js
@@ -0,0 +1,20 @@
+$(function() {
+  // Initialize daterangepicker
+  $("#daterange").daterangepicker();
+
+  // Initialize Swiper
+  var swiper = new Swiper(".swiper-container", {
+    slidesPerView: 4,
+    slidesPerGroup: 1,
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+    },
+  });
+});
+
+$(".js-reserveControl").click(function (e) {
+  e.preventDefault();
+  $(".js-reserveTotal").toggleClass("d-none");
+  $(".js-reserveIcon").toggleClass("rotate180");
+});
